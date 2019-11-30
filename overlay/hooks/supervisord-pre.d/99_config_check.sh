05fb9b59d11587c63a0c7bd542bf80f38e61d688
Stuart McCulloch Anderson <nxfifteen@users.noreply.github.com>
2019-11-30 19:59:38 +0000

    Update 99_config_check.sh
    Reverted back to base

--- overlay/hooks/supervisord-pre.d/99_config_check.sh
+++ overlay/hooks/supervisord-pre.d/99_config_check.sh
@@ -4,7 +4,6 @@ echo "Currently configured config:"
 /scripts/getconfig.sh /etc/nginx/nginx.conf
 
 echo "Checking nginx config"
-#/usr/sbin/nginx -t
+/usr/sbin/nginx -t
 
-# [ $? -ne 0 ] || echo "Config check successful"
-echo "Config check successful"
+ [ $? -ne 0 ] || echo "Config check successful"
